1d4a92df06115bc0812cd3f1f770bcacd7d68172
Nik Stern <nik.stern@datadoghq.com>
2026-08-26 07:07:08 -0700

    test(paw-fs): reproduce typed metadata restart failure

---
+++ crates/temperpaw/tests/fixtures/pawfs-typed-client/src/lib.rs
@@ -0,0 +1,9 @@
+include!(concat!(env!("OUT_DIR"), "/pawfs_file_client.rs"));
+
+pub const MANIFEST_JSON: &str =
+    include_str!(concat!(env!("OUT_DIR"), "/pawfs_file_client_manifest.json"));
+
+pub use temper_wasm_sdk::data::{
+    DataResponseV1, DataResultV1, ModuleSdkManifest, install_native_data_host_for_test,
+    take_native_data_requests_for_test,
+};
